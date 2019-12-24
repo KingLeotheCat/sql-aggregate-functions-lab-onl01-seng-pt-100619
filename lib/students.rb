b63b218520df016458d2cdca470f658873163829
Lenny Lee <lennylee91@gmail.com>
2019-12-24 07:09:37 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -14,7 +14,7 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-  "SELECT COUNT(*) FROM tardies;"
+  "SELECT COUNT(tardies) FROM students WHERE 1;"
 end
 
 def average_gpa_for_9th_grade
