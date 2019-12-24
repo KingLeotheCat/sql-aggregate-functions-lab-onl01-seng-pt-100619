f7812210e87a87112a005fd629a2e580802a353c
Lenny Lee <lennylee91@gmail.com>
2019-12-24 07:03:47 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -6,13 +6,17 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
+  "SELECT MIN(gpa) FROM students"
 end
 
 def average_student_gpa
+  "SELECT AVG FROM students"
 end
 
 def total_tardies_for_all_students
+  "SELECT COUNT(tardies) FROM students"
 end
 
 def average_gpa_for_9th_grade
+  "SELECT AVG(gpa) FROM grade"
 end
